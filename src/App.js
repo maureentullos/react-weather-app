eebc7c007b3e68c5684eb5fb66dbe0295d675cd3
maureentullos <Mswalsh220@gmail.com>
2023-12-29 15:07:14 -0500

    Linked to github and started content on page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Maureen Tullos and is
+        <a
+          href="https://github.com/maureentullos/react-weather-app.git"
+          target="_blank"
+        > open-sourced on Github.
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
