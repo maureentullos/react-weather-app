95dd1165d572b591a091a8781c353ec4824f841f
maureentullos <Mswalsh220@gmail.com>
2023-12-30 13:59:00 -0500

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Maureen Tullos and is
-        <a
-          href="https://github.com/maureentullos/react-weather-app.git"
-          target="_blank"
-        > open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Maureen Tullos and is
+          <a
+            href="https://github.com/maureentullos/react-weather-app.git"
+            target="_blank" rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
